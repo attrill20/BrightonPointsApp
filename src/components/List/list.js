3f28e692d842cee67e2e441e86395d194e9a84d3
James Attrill <attrill20@gmail.com>
2024-11-02 15:41:34 +0000

    added new stats box

--- src/components/List/list.js
+++ src/components/List/list.js
@@ -84,6 +84,261 @@ const List = ({ mainData, activeGameweek, selectedGameweek, onGameweekChange, ja
         return 0;
     };
 
+    const getPlayerMinutes = (playerId) => {
+        if (gameweekData) {
+            const playerStats = gameweekData.elements.find(player => player.id === playerId);
+            return playerStats ? playerStats.stats.minutes : 0;
+        }
+    };
+
+    const getMinutesPoints = (playerId) => {
+        const minutes = getPlayerMinutes(playerId);
+        
+        if (minutes === 0) return 0;
+        if (minutes >= 1 && minutes <= 59) return 1;
+        if (minutes >= 60) return 2;
+    };
+
+    const getPlayerGoals = (playerId) => {
+        if (gameweekData) {
+            const playerStats = gameweekData.elements.find(player => player.id === playerId);
+            return playerStats ? playerStats.stats.goals_scored : 0;
+        }
+    };
+
+    const getGoalsPoints = (playerId) => {
+        const player = elements.find(player => player.id === playerId);
+        const goals = getPlayerGoals(playerId);
+    
+        if (!player || goals === 0) return 0;
+    
+        let goalPointsPerGoal;
+        switch (player.element_type) {
+            case 1: // Goalkeepers
+            case 2: // Defenders
+                goalPointsPerGoal = 6;
+                break;
+            case 3: // Midfielders
+                goalPointsPerGoal = 5;
+                break;
+            case 4: // Forwards
+                goalPointsPerGoal = 4;
+                break;
+            default:
+                goalPointsPerGoal = 0;
+                break;
+        }
+    
+        return goals * goalPointsPerGoal;
+    };
+    
+    const getPlayerAssists = (playerId) => {
+        if (gameweekData) {
+            const playerStats = gameweekData.elements.find(player => player.id === playerId);
+            return playerStats ? playerStats.stats.assists : 0;
+        }
+    };
+
+    const getAssistsPoints = (playerId) => {
+        const player = elements.find(player => player.id === playerId);
+        const assists = getPlayerAssists(playerId);
+    
+        if (!player || assists === 0) return 0;
+    
+        let assistsPoints = 3;
+    
+        return assists * assistsPoints;
+    };
+
+    const getPlayerCleanSheets = (playerId) => {
+        if (gameweekData) {
+            const playerStats = gameweekData.elements.find(player => player.id === playerId);
+            return playerStats ? playerStats.stats.clean_sheets : 0;
+        }
+    };
+
+    const getCleanSheetPoints = (playerId) => {
+        const player = elements.find(player => player.id === playerId);
+        const cleanSheets = getPlayerCleanSheets(playerId);
+    
+        if (!player || cleanSheets === 0) return 0;
+    
+        let cleanSheetPointsPerCleanSheet;
+        switch (player.element_type) {
+            case 1: // Goalkeepers
+            case 2: // Defenders
+                cleanSheetPointsPerCleanSheet = 4;
+                break;
+            case 3: // Midfielders
+                cleanSheetPointsPerCleanSheet = 1;
+                break;
+            default:
+                cleanSheetPointsPerCleanSheet = 0;
+                break;
+        }
+    
+        return cleanSheets * cleanSheetPointsPerCleanSheet;
+    };
+
+    const getPlayerPenaltySaves = (playerId) => {
+        if (gameweekData) {
+            const playerStats = gameweekData.elements.find(player => player.id === playerId);
+            return playerStats ? playerStats.stats.penalties_saved : 0;
+        }
+    };
+
+    const getPenaltySavesPoints = (playerId) => {
+        const player = elements.find(player => player.id === playerId);
+        const penaltySaves = getPlayerPenaltySaves(playerId);
+    
+        if (!player || penaltySaves === 0) return 0;
+    
+        let penaltySavesPoints = 5;
+    
+        return penaltySaves * penaltySavesPoints;
+    };
+
+    const getPlayerSaves = (playerId) => {
+        if (gameweekData) {
+            const playerStats = gameweekData.elements.find(player => player.id === playerId);
+            return playerStats ? playerStats.stats.saves : 0;
+        }
+    };
+
+    const getSavesPoints = (playerId) => {
+        const player = elements.find(player => player.id === playerId);
+        const saves = getPlayerSaves(playerId);
+    
+        if (!player || saves === 0) return 0;
+    
+        const savesPoints = Math.floor(saves / 3);
+
+        return savesPoints;
+    };
+
+    const getPlayerOwnGoals = (playerId) => {
+        if (gameweekData) {
+            const playerStats = gameweekData.elements.find(player => player.id === playerId);
+            return playerStats ? playerStats.stats.own_goals : 0;
+        }
+    };
+
+    const getOwnGoalsPoints = (playerId) => {
+        const player = elements.find(player => player.id === playerId);
+        const ownGoals = getPlayerOwnGoals(playerId);
+    
+        if (!player || ownGoals === 0) return 0;
+    
+        return ownGoals * -2;
+    };
+
+    const getPlayerPenaltiesMissed = (playerId) => {
+        if (gameweekData) {
+            const playerStats = gameweekData.elements.find(player => player.id === playerId);
+            return playerStats ? playerStats.stats.penalties_missed : 0;
+        }
+    };
+
+    const getPenaltiesMissedPoints = (playerId) => {
+        const player = elements.find(player => player.id === playerId);
+        const penaltiesMissed = getPlayerPenaltiesMissed(playerId);
+    
+        if (!player || penaltiesMissed === 0) return 0;
+    
+        return penaltiesMissed * -2;
+    };
+
+    const getPlayerGoalsConceded = (playerId) => {
+        if (gameweekData) {
+            const playerStats = gameweekData.elements.find(player => player.id === playerId);
+            return playerStats ? playerStats.stats.goals_conceded : 0;
+        }
+    };
+
+    const getGoalsConcededPoints = (playerId) => {
+        const player = elements.find(player => player.id === playerId);
+        const goalsConceded = getPlayerGoalsConceded(playerId);
+    
+        if (!player || goalsConceded === 0) return 0;
+    
+        let goalsConcededPoints;
+        switch (player.element_type) {
+            case 1: // Goalkeepers
+            case 2: // Defenders
+                goalsConcededPoints = -Math.floor(goalsConceded / 2);
+                break;
+            default:
+                goalsConcededPoints = 0;
+                break;
+        }
+    
+        return goalsConcededPoints;
+    };
+
+    const getPlayerYellowCards = (playerId) => {
+        if (gameweekData) {
+            const playerStats = gameweekData.elements.find(player => player.id === playerId);
+            return playerStats ? playerStats.stats.yellow_cards : 0;
+        }
+    };
+
+    const getYellowCardsPoints = (playerId) => {
+        const player = elements.find(player => player.id === playerId);
+        const yellowCards = getPlayerYellowCards(playerId);
+    
+        if (!player || yellowCards === 0) return 0;
+    
+        return yellowCards * -1;
+    };
+
+    const getPlayerRedCards = (playerId) => {
+        if (gameweekData) {
+            const playerStats = gameweekData.elements.find(player => player.id === playerId);
+            return playerStats ? playerStats.stats.red_cards : 0;
+        }
+    };
+
+    const getRedCardsPoints = (playerId) => {
+        const player = elements.find(player => player.id === playerId);
+        const redCards = getPlayerRedCards(playerId);
+    
+        if (!player || redCards === 0) return 0;
+    
+        return redCards * -3;
+    };
+
+    const getPlayerBPS = (playerId) => {
+        if (gameweekData) {
+            const playerStats = gameweekData.elements.find(player => player.id === playerId);
+            return playerStats ? playerStats.stats.bps : 0;
+        }
+    };
+
+    const getBPSPoints = (playerId) => {
+        const player = elements.find(player => player.id === playerId);
+        const bps = getPlayerBPS(playerId);
+    
+        if (!player || bps === 0) return 0;
+    
+        return bps;
+    };
+
+    const getPlayerBonus = (playerId) => {
+        if (gameweekData) {
+            const playerStats = gameweekData.elements.find(player => player.id === playerId);
+            return playerStats ? playerStats.stats.bonus : 0;
+        }
+    };
+
+    const getBonusPoints = (playerId) => {
+        const player = elements.find(player => player.id === playerId);
+        const bonus = getPlayerBonus(playerId);
+    
+        if (!player || bonus === 0) return 0;
+    
+        return bonus;
+    };
+
     const handleGameweekInputChange = (e) => {
         const value = e.target.value;
 
@@ -130,7 +385,6 @@ const List = ({ mainData, activeGameweek, selectedGameweek, onGameweekChange, ja
         setMultiplier((prevMultiplier) => Math.max(prevMultiplier - 1, 0));
     };
     
-
     const totalPointsJames = playersForJames.reduce((sum, player) => sum + getPlayerPoints(player.id), 0);
     const totalPointsLaurie = playersForLaurie.reduce((sum, player) => sum + getPlayerPoints(player.id), 0);
 
@@ -144,6 +398,66 @@ const List = ({ mainData, activeGameweek, selectedGameweek, onGameweekChange, ja
             return "It's a draw!";
         }
     };
+
+    const PlayerColumn = ({ title, players, totalPoints, getPlayerPoints, getPlayerMinutes, getMinutesPoints, getPlayerGoals, getGoalsPoints, getPlayerAssists, getAssistsPoints }) => (
+        <div className="player-column">
+            <p className="column-title"><strong>{title} Players</strong></p>
+            <p className="total-points">Total Points: <strong>{totalPoints}</strong></p>
+            {players.length > 0 ? (
+                <div className="pics-wrapper">
+                    {players.map((player, index) => (
+                        <div key={player.code} className="player-pic-container">
+                            <img
+                                className="player-pic"
+                                src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player.code}.png`}
+                                onError={(e) => { e.target.src = "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_36-110.png"; }}
+                                alt={`player-${index + 1}`}
+                            />
+                            <p className="player-stat-name">{player.web_name}: <strong>{getPlayerPoints(player.id)}</strong></p>
+                            <p className="player-stat-box">
+                                <p>Minutes: {getPlayerMinutes(player.id)} <strong>[{getMinutesPoints(player.id)}]</strong></p>
+                                {getPlayerGoals(player.id) !== 0 && (
+                                    <p>Goals: {getPlayerGoals(player.id)} <strong>[{getGoalsPoints(player.id)}]</strong></p>
+                                )}
+                                {getPlayerAssists(player.id) !== 0 && (
+                                    <p>Assists: {getPlayerAssists(player.id)} <strong>[{getAssistsPoints(player.id)}]</strong></p>
+                                )}
+                                {getCleanSheetPoints(player.id) !== 0 && (
+                                    <p>Clean Sheets: {getPlayerCleanSheets(player.id)} <strong>[{getCleanSheetPoints(player.id)}]</strong></p>
+                                )}
+                                {getPlayerPenaltySaves(player.id) !== 0 && (
+                                    <p>Penalty Saves: {getPlayerPenaltySaves(player.id)} <strong>[{getPenaltySavesPoints(player.id)}]</strong></p>
+                                )}
+                                {getPlayerSaves(player.id) !== 0 && (
+                                    <p>Saves: {getPlayerSaves(player.id)} <strong>[{getSavesPoints(player.id)}]</strong></p>
+                                )}
+                                {getGoalsConcededPoints(player.id) !== 0 && (
+                                    <p>Conceded: {getPlayerGoalsConceded(player.id)} <strong>[{getGoalsConcededPoints(player.id)}]</strong></p>
+                                )}
+                                {getPlayerOwnGoals(player.id) !== 0 && (
+                                    <p>Own Goals: {getPlayerOwnGoals(player.id)} <strong>[{getOwnGoalsPoints(player.id)}]</strong></p>
+                                )}
+                                {getPlayerPenaltiesMissed(player.id) !== 0 && (
+                                    <p>Penalties Missed: {getPlayerPenaltiesMissed(player.id)} <strong>[{getPenaltiesMissedPoints(player.id)}]</strong></p>
+                                )}
+                                {getPlayerYellowCards(player.id) !== 0 && (
+                                    <p>Yellow Cards: {getPlayerYellowCards(player.id)} <strong>[{getYellowCardsPoints(player.id)}]</strong></p>
+                                )}
+                                {getPlayerRedCards(player.id) !== 0 && (
+                                    <p>Red Cards: {getPlayerRedCards(player.id)} <strong>[{getRedCardsPoints(player.id)}]</strong></p>
+                                )}
+                                {getPlayerBPS(player.id) !== 0 && (
+                                    <p>BPS: {getPlayerBPS(player.id)} <strong>[{getBonusPoints(player.id)}]</strong></p>
+                                )}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <p>No players found for {title}</p>
+            )}
+        </div>
+    );
 
     return (
         <div>
@@ -180,51 +494,44 @@ const List = ({ mainData, activeGameweek, selectedGameweek, onGameweekChange, ja
                 </div>
             </div>
 
-
             <div className="player-columns">
-                <div className="player-column">
-                    <p className="column-title"><strong>James Players</strong></p>
-                    <p className="total-points">Total Points: <strong>{totalPointsJames}</strong></p>
-                    {playersForJames.length > 0 ? (
-                        <div className="pics-wrapper">
-                            {playersForJames.map((player, index) => (
-                                <div key={player.code} className="player-pic-container">
-                                    <img
-                                        className="player-pic"
-                                        src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player.code}.png`}
-                                        onError={(e) => { e.target.src = "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_36-110.png"; }}
-                                        alt={`player-${index + 1}`}
-                                    />
-                                    <p className="player-stat-name">{player.web_name}: <strong>{getPlayerPoints(player.id)}</strong></p>
-                                </div>
-                            ))}
-                        </div>
-                    ) : (
-                        <p>No players found for James</p>
-                    )}
-                </div>
+                <PlayerColumn
+                    title="James"
+                    players={playersForJames}
+                    totalPoints={totalPointsJames}
+                    getPlayerPoints={getPlayerPoints}
+                    getPlayerMinutes={getPlayerMinutes}
+                    getMinutesPoints={getMinutesPoints}
+                    getPlayerGoals={getPlayerGoals}
+                    getGoalsPoints={getGoalsPoints}
+                    getPlayerAssists={getPlayerAssists}
+                    getAssistsPoints={getAssistsPoints}
+                    getPlayerCleanSheets={getPlayerCleanSheets}
+                    getCleanSheetPoints={getCleanSheetPoints}
+                    getPlayerSaves={getPlayerSaves}
+                    getSavesPoints={getSavesPoints}
+                    getPlayerGoalsConceded={getPlayerGoalsConceded}
+                    getGoalsConcededPoints={getGoalsConcededPoints}
+                />
 
-                <div className="player-column">
-                    <p className="column-title"><strong>Laurie Players</strong></p>
-                    <p className="total-points">Total Points: <strong>{totalPointsLaurie}</strong></p>
-                    {playersForLaurie.length > 0 ? (
-                        <div className="pics-wrapper">
-                            {playersForLaurie.map((player, index) => (
-                                <div key={player.code} className="player-pic-container">
-                                    <img
-                                        className="player-pic"
-                                        src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player.code}.png`}
-                                        onError={(e) => { e.target.src = "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_36-110.png"; }}
-                                        alt={`player-${index + 1}`}
-                                    />
-                                    <p className="player-stat-name">{player.web_name}: <strong>{getPlayerPoints(player.id)}</strong></p>
-                                </div>
-                            ))}
-                        </div>
-                    ) : (
-                        <p>No players found for Laurie</p>
-                    )}
-                </div>
+                <PlayerColumn
+                    title="Laurie"
+                    players={playersForLaurie}
+                    totalPoints={totalPointsLaurie}
+                    getPlayerPoints={getPlayerPoints}
+                    getPlayerMinutes={getPlayerMinutes}
+                    getMinutesPoints={getMinutesPoints}
+                    getPlayerGoals={getPlayerGoals}
+                    getGoalsPoints={getGoalsPoints}
+                    getPlayerAssists={getPlayerAssists}
+                    getAssistsPoints={getAssistsPoints}
+                    getPlayerCleanSheets={getPlayerCleanSheets}
+                    getCleanSheetPoints={getCleanSheetPoints}
+                    getPlayerSaves={getPlayerSaves}
+                    getSavesPoints={getSavesPoints}
+                    getPlayerGoalsConceded={getPlayerGoalsConceded}
+                    getGoalsConcededPoints={getGoalsConcededPoints}
+                />
             </div>
         </div>
     );
